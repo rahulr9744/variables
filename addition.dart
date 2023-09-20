8e1857029e84781f22429dbbd9028667be3bf15f
rahulr9744 <rahulramesh0070@gmail.com>
2023-09-20 13:08:31 +0530

    commit

---
+++ addition.dart
@@ -0,0 +1,13 @@
+void addition() {
+
+  int a = 100,
+      b = 250;
+
+  print('sum = ${a+b}');
+
+}
+
+void main() {
+  addition();
+}
+
---
+++ user_input.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+void main (){
+  print('enter your name');
+  String? name = stdin.readLineSync();
+  print('enter your age');
+  int age = int.parse(stdin.readLineSync()!);
+   print('enter your cgpa');
+   double mark = double.parse(stdin.readLineSync()!);
+   print("name= $name");
+   print("age = $age");
+   print("mark =$mark");
+
+   
+
+
+  
+  
+}
